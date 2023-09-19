bb527860f24fe5c40012b7f5dca89a95e0119e45
Soujanya S <109354926+Soujanyasalonkar@users.noreply.github.com>
2023-09-19 12:24:33 +0530

    Create f_sequence.sv

---
+++ f_sequence.sv
@@ -0,0 +1,55 @@
+class f_sequence extends uvm_sequence#(f_sequence_item);
+  `uvm_object_utils(f_sequence)
+  
+  function new(string name = "f_sequence");
+    super.new(name);
+  endfunction
+  
+  virtual task body();
+        
+    
+    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Write REQs ********"), UVM_LOW)
+    repeat(10) begin
+      req = f_sequence_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize() with {i_wren == 1;});
+      finish_item(req);
+    end
+    
+   #5;
+    
+    `uvm_info(get_type_name(), $sformatf("******** Generate 1024 Read REQs ********"), UVM_LOW)
+    repeat(10) begin
+      req = f_sequence_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize() with {i_rden == 1;});
+      finish_item(req);
+    end
+    #5;
+    
+    `uvm_info(get_type_name(), $sformatf("******** Generate 20 Random REQs ********"), UVM_LOW)
+    repeat(20) begin
+      req = f_sequence_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize());
+      finish_item(req);
+    end
+    
+  //  #5;
+    
+    /*`uvm_info(get_type_name(), $sformatf("******** Generate 20 alternate REQs ********"), UVM_LOW)
+    repeat(10) begin
+      req = f_sequence_item::type_id::create("req");
+      start_item(req);
+      assert(req.randomize()  with {i_wren == 1;});
+      finish_item(req);
+      start_item(req);
+      assert(req.randomize()  with {i_rden == 1;});
+      finish_item(req);
+    end  */
+  
+  endtask
+  
+endclass
+  
+  
